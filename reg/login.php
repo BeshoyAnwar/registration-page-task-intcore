273b0114792dbe9137527e4b74cb17d2fa4f287a
Beshoy Anwar <32778960+BeshoyAnwar@users.noreply.github.com>
2019-01-25 19:39:38 +0200

    Update login.php

--- reg/login.php
+++ reg/login.php
@@ -45,25 +45,6 @@
 		}	
 	
 ?>
-# registration-page-task-intcore
-## Features
-1-Registration page with following data: name, email, password, photo.<br />
-2-Password should be *Hashed* and validations is provided.<br />
-3-Login form with email and password addition to remember me checkbox.<br />
-4-After login is done then redirecting to update profile page to update all information name, email, password, photo.<br />
-5-User can logout from his account.<br />
-## Used Tools
-Task is implemented via php and mysql database using xampp sever.
-## How to start
-1-Setup [xampp](http://mrbool.com/how-to-install-xampp-server-in-windows/28257)<br />
-2-Create in the mysql accounts database,you may set a password and change it in dbcontroller.php file<br />
-3-Download[Souce code] and put it in the path:"c:\xampp\htdoc\"<br />
-4-from any browser goto "http://localhost/reg/form.php"<br />
-Registration page with following data [name, email, password, photo] with login form with email and password addition to remember me checkbox implemented using php and mysql database
-registeration-page-task-intcore
-
-
-
 
 <link href="//db.onlinewebfonts.com/c/a4e256ed67403c6ad5d43937ed48a77b?family=Core+Sans+N+W01+35+Light" rel="stylesheet" type="text/css"/>
 <link rel="stylesheet" href="form.css" type="text/css">
