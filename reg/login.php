eb1262d631befae78709f07818bbde222e2522e0
Beshoy Anwar <32778960+BeshoyAnwar@users.noreply.github.com>
2019-01-25 22:40:10 +0200

    Update login.php

--- reg/login.php
+++ reg/login.php
@@ -43,7 +43,7 @@
 		else{
 			$_SESSION['message']="Email is not registered";
 		}	
-	
+	}
 ?>
 
 <link href="//db.onlinewebfonts.com/c/a4e256ed67403c6ad5d43937ed48a77b?family=Core+Sans+N+W01+35+Light" rel="stylesheet" type="text/css"/>
